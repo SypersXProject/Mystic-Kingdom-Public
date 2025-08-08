8c709838d61233e34b9438abbe27b77cda2066a3
SypersXProject <syneyzx@gmail.com>
2025-08-08 22:35:13 +0700

    Update AF.lua

--- Anti Cheat/AF.lua
+++ Anti Cheat/AF.lua
@@ -1,34 +1,33 @@
-local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-local PlayerInfos = {}
-local MaxAirborneTime = 2
-RunService.Heartbeat:Connect(function(Delta)
-	for _, Player in pairs(Players:GetPlayers()) do 
-		local Character = Player.Character
-		if not Character then 
-			continue
-		end
-		local Humanoid:Humanoid = Character:FindFirstChild("Humanoid")
-		if not Humanoid then
-			continue
-		end
-		local Info = PlayerInfos[Player.Name] 
-		if not Info then
-			PlayerInfos[Player.Name] = {
-				LastCFrameOnGround = Character:GetPivot(), 
+local a = game:GetService(string.char(82,117,110,83,101,114,118,105,99,101)) 
+local b = game:GetService(string.char(80,108,97,121,101,114,115))
+local c = {}
+local d = 2
+a.Heartbeat:Connect(function(e)
+	for _, f in pairs(b:GetPlayers()) do
+		local g = f.Character
+		if not g then continue end
+
+		local h = g:FindFirstChild(string.char(72,117,109,97,110,111,105,100))
+		if not h then continue end
+
+		local i = c[f.Name]
+		if not i then
+			c[f.Name] = {
+				LastCFrameOnGround = g:GetPivot(),
 				TimeAirborne = 0
 			}
 			continue
 		end
-		if Humanoid.FloorMaterial == Enum.Material.Air then 
-			PlayerInfos[Player.Name].TimeAirborne += Delta
-			if PlayerInfos[Player.Name].TimeAirborne > MaxAirborneTime then
-				PlayerInfos[Player.Name].TimeAirborne = 0
-				Character:PivotTo(Info.LastCFrameOnGround)
+
+		if h[string.char(70,108,111,111,114,77,97,116,101,114,105,97,108)] == Enum.Material.Air then -- "FloorMaterial"
+			c[f.Name].TimeAirborne += e
+			if c[f.Name].TimeAirborne > d then
+				c[f.Name].TimeAirborne = 0
+				g:PivotTo(i.LastCFrameOnGround)
 			end
 		else
-			PlayerInfos[Player.Name].TimeAirborne = 0
-			PlayerInfos[Player.Name].LastCFrameOnGround = Character:GetPivot()
+			c[f.Name].TimeAirborne = 0
+			c[f.Name].LastCFrameOnGround = g:GetPivot()
 		end
 	end
 end)
